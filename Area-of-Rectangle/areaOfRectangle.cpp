127459fdfee1f054e1ea25404b4ce4d3bd9c1c6c
Riya Savant <rieee2110@gmail.com>
2021-02-04 08:10:45 +0530

    Modified solution: Taking all inputs at once

--- Area-of-Rectangle/areaOfRectangle.cpp
+++ Area-of-Rectangle/areaOfRectangle.cpp
@@ -12,6 +12,7 @@ struct coordinate
 struct rectangle
 {
     struct coordinate p[3];
+    float area;
 };
 
 int inputSize()
@@ -21,12 +22,26 @@ int inputSize()
     cin >> n;
     return n;
 }
-struct rectangle inputCoordinates()
+
+// Single input
+// struct rectangle inputCoordinates()
+// {
+//     struct rectangle r;
+//     cout << "Enter the coordinates:\n";
+//     scanf("%f %f %f %f %f %f", &r.p[0].x, &r.p[0].y, &r.p[1].x, &r.p[1].y, &r.p[2].x, &r.p[2].y);
+//     return r;
+// }
+
+// Array of Inputs
+
+void inputCoordinates(int n, struct rectangle r[])
 {
-    struct rectangle r;
-    cout << "Enter the coordinates:\n";
-    scanf("%f %f %f %f %f %f", &r.p[0].x, &r.p[0].y, &r.p[1].x, &r.p[1].y, &r.p[2].x, &r.p[2].y);
-    return r;
+    cout << "Enter three points for each rectangle\n";
+    for (int i = 0; i < n; i++)
+    {
+        printf("Rectangle %d: ", i + 1);
+        scanf("%f %f %f %f %f %f", &r[i].p[0].x, &r[i].p[0].y, &r[i].p[1].x, &r[i].p[1].y, &r[i].p[2].x, &r[i].p[2].y);
+    }
 }
 
 int computeDistance(struct coordinate p1, struct coordinate p2)
@@ -34,11 +49,34 @@ int computeDistance(struct coordinate p1, struct coordinate p2)
     return sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
 }
 
-float computeArea(struct rectangle r)
+// Calculating area of single rectangle
+// float computeArea(struct rectangle r)
+// {
+//     float length = computeDistance(r.p[0], r.p[1]);
+//     float breadth = computeDistance(r.p[0], r.p[2]);
+//     return length * breadth;
+// }
+
+// Calculating area of array of rectangles
+void computeArea(int n, struct rectangle r[])
 {
-    float length = computeDistance(r.p[0], r.p[1]);
-    float breadth = computeDistance(r.p[0], r.p[2]);
-    return length * breadth;
+    float length, breadth;
+    for (int i = 0; i < n; i++)
+    {
+        length = computeDistance(r[i].p[0], r[i].p[1]);
+        breadth = computeDistance(r[i].p[0], r[i].p[2]);
+
+        r[i].area = length * breadth;
+    }
+}
+
+void output(int n, struct rectangle r[])
+{
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Area of rectangle with vertices (%.1f, %.1f), (%.1f, %.1f), (%.1f, %.1f) is %.1f\n", r[i].p[0].x, r[i].p[0].y, r[i].p[1].x, r[i].p[1].y, r[i].p[2].x, r[i].p[2].y, r[i].area);
+    }
 }
 
 int main()
@@ -46,13 +84,18 @@ int main()
     int n;
     n = inputSize();
 
+    struct rectangle r[n];
+    inputCoordinates(n, r);
+    computeArea(n, r);
+    output(n, r);
+
     // Taking one input at a time
-    while (n > 0)
-    {
-        struct rectangle r;
-        r = inputCoordinates();
-        printf("Area: %.1f\n", computeArea(r));
-        n--;
-    }
+    // while (n > 0)
+    // {
+    //     struct rectangle r;
+    //     r = inputCoordinates();
+    //     printf("Area: %.1f\n", computeArea(r));
+    //     n--;
+    // }
     return 0;
 }
